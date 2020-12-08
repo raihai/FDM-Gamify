203c56d33dd7e026d9d85b22f317e7549f7baaec
adam <b9014581@newcastle.ac.uk>
2020-12-08 18:27:32 +0000

    Game program started, kind of a buggy mess. Proceed at own risk

---
+++ AstraModel.cs
@@ -0,0 +1,7 @@
+﻿namespace fdm_gamify2
+{
+    public class AstraModel
+    {
+        public int DistFromGround { get; set; } = 100;
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+
+namespace fdm_gamify2
+{
+    public class GameManager : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public AstraModel Astra { get; set; }
+        public bool IsRunning;
+
+        public GameManager()
+        {
+            Astra = new AstraModel();
+        }
+
+        public async void MainLoop()
+        {
+            IsRunning = true;
+            while (IsRunning)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Astra)));
+
+                if (Astra.DistFromGround <= 0)
+                {
+                    GameOver();
+                }
+            }
+        }
+
+        public void StartGame()
+        {
+            if (!IsRunning)
+            {
+                Astra = new AstraModel();
+                MainLoop();
+            }
+            
+        }
+
+        public void GameOver()
+        {
+            IsRunning = false;
+        }
+    }
+}
