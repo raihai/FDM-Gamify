8c37de8eb1e67198b1ea555c15f5e0647dcfaaba
adam <b9014581@newcastle.ac.uk>
2020-12-10 12:32:33 +0000

    Added a Player class

---
+++ Player.cs
@@ -0,0 +1,34 @@
+﻿namespace fdm_gamify2
+{
+    public class Player
+    {
+        private string _nickname;
+        private int _score;
+        
+        public Player(string nickname, int score)
+        {
+            _nickname = nickname;
+            _score = score;
+        }
+
+        public string GetNickname()
+        {
+            return _nickname;
+        }
+
+        public void SetNickname(string nickname)
+        {
+            _nickname = nickname;
+        }
+
+        public int GetScore()
+        {
+            return _score;
+        }
+
+        public void SetScore(int score)
+        {
+            _score = score;
+        }
+    }
+}
