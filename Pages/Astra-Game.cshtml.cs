286105cae0e9bdcd83b53ccfdc809b153c48b7ab
b9012721 <Intensetryterms1>
2020-12-14 11:04:16 +0000

    Everything fixed I think?

---
+++ Pages/Astra-Game.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fdm_gamify2.Pages
+{
+    public class Test_GameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
