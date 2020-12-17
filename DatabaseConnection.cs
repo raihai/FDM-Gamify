2220ad7134732a38b1dab7877ff4f6a9d99c3b2b
b9012721 <Intensetryterms1>
2020-12-17 09:51:49 +0000

    Created SSH tunnel to linux server then port forwarded to the mysql server. Connection now established.

--- DatabaseConnection.cs
+++ DatabaseConnection.cs
@@ -1,21 +1,32 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Data.SqlClient;
-
+using System.Net;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.AspNetCore.SignalR;
+using MySql.Data.MySqlClient;
+using Renci.SshNet;
+// uses mysql.data in NuGet
+//uses Ssh.net install from https://github.com/sshnet/SSH.NET
 namespace fdm_gamify2
 {
     public class DatabaseConnection
     {
+
         // connection variables
         // when testing be sure to change connection string to suit your local database
         // perhaps create your own ConString and comment out whichever is not yours
-        private const string ConString = "Data Source=csc2033@ocalhost;Initial Catalog=csc2033;User id=root;Password=SqlPwd01";
-        private SqlConnection _connection;
+        
+        private const string ConString = "Data Source=cs-db.ncl.ac.uk;Initial Catalog=t2033t26;User id=t2033t26;Password=Sit-HewsRide";
+        private MySqlConnection _connection;
 
         // opens connection to database
         public void OpenConnection()
         {
-            _connection = new SqlConnection(ConString);
-            _connection.Open();
+            Console.WriteLine("Start of open connection");
+            SshClient client = SSHTunnel();
+            Console.Write("reached end of opening connection");
+
         }
 
         // close connection to database
@@ -27,15 +38,15 @@ namespace fdm_gamify2
         // executes a given query
         public void ExecuteQuery(string query)
         {
-            SqlCommand cmd = new SqlCommand(query, _connection);
+            MySqlCommand cmd = new MySqlCommand(query, _connection);
             cmd.ExecuteNonQuery();
         }
 
         // reads forward-only stream of rows database
-        public SqlDataReader DataReader(string query)
+        public void DataReader(string query)
         {
-            SqlCommand cmd = new SqlCommand(query, _connection);
-            return cmd.ExecuteReader();
+            MySqlCommand cmd = new MySqlCommand(query, _connection); ;
+            
         }
 
         // shows data returned by query in a grid view
@@ -46,5 +57,53 @@ namespace fdm_gamify2
             adapter.Fill(dataSet);
             return dataSet.Tables[0];
         }
+
+        /*
+         *Creates SSH Tunnel to linux server
+         * Replace Username and Passowrd in connection info variable to your own.
+         * Once connected connects to shared db using a port from local host -> cs.db
+         * Connects to SQL DB
+         * 
+         */
+        public SshClient SSHTunnel()
+        {
+            string username = "";
+            string password = "";
+            Console.WriteLine("Start of method");
+            var connectionInfo = new SshClient("cs-linux.ncl.ac.uk",
+                username, password); 
+            
+            using (var client = connectionInfo)
+            {
+                client.Connect();
+                if (client.IsConnected)
+                {
+                    Console.Write("hanging...");
+                    //client.RunCommand("mysql -h cs-db.ncl.ac.uk -u t2033t26 -p Sit-HewsRide");
+                    Console.Write("done");
+                    var portForwarded = new ForwardedPortLocal("localhost", 3306, "cs-db.ncl.ac.uk", 3306);;
+                    Console.WriteLine(portForwarded.IsStarted);
+                    client.AddForwardedPort(portForwarded);
+                    portForwarded.Start();
+                    Console.Write(portForwarded.IsStarted);
+                    MySqlConnection con = new MySqlConnection(
+                            "SERVER= localhost;UID=t2033t26;PASSWORD=Sit-HewsRide;DATABASE=t2033t26");// server is equal to the first host variable in the port above ^^
+                    con.Open();
+                    Console.Write("opened");
+                    MySqlCommand com = new MySqlCommand("SELECT * FROM Persons", con);// just a test command
+                    MySqlDataReader rdr = com.ExecuteReader();
+                    while (rdr.Read())// reads through enumerable 
+                    {
+                        Console.WriteLine(rdr[1]);
+                    }
+                    rdr.Close();
+                    return client; 
+
+                }
+                Console.Write("client not connected");
+            }
+
+            return null;
+        }
     }
-}
+    }
