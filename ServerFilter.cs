b769492f64f4a8d4386934334229f91abebe5d5f
Glen <g.p.arch1@newcastle.ac.uk>
2021-01-05 11:54:09 +0000

    Created the server filter that we can attach to any user input

---
+++ ServerFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace fdm_gamify2
+{
+    public class ServerFilter
+    {
+        public static Boolean Filter(String Value)
+        {
+            String[] BadInputs = {"<",">","!","{","}","insert","where","script","delete","input"};
+            foreach (String Input in BadInputs)
+            {
+                if (Input == Value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
