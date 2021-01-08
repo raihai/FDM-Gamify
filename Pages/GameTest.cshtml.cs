5e3fb615d6e89332162033d7248603ed42e88b3f
b9012721 <Intensetryterms1>
2021-01-08 10:19:52 +0000

    Added C# to gametest

---
+++ Pages/GameTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fdm_gamify2.Pages
+{
+    public class GameTest : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
