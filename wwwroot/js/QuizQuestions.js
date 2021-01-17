febfdebc2b0a29dc743eb03ec3ed5de0e653fb3e
Rajbol Rai <r.rai1@newcastle.ac.uk>
2021-01-17 10:59:28 +0000

    csv file reader works, however, selectOption doesn't

--- wwwroot/js/QuizQuestions.js
+++ wwwroot/js/QuizQuestions.js
@@ -1,92 +1,74 @@
-﻿
-// get data not working at the moment 
+﻿/*const quest = [];
+console.log(quest);
+const option1 = [];
+const option2 =[];
+const option3 = [];
+const ans = [];
+console.log(ans);
+
+
+
 async function getData(){
     const response = await  fetch('quizQuestionTest.csv');
     const data = await response.text()
-    const rows = data.split('\n').slice(1);
+    const rows = data.split('\n');
     rows.forEach( element => {
-        const row = element.split(',').toLocaleString();
-        const que = row[0];
-        const option1 = row[1];
-        const option2 = row[2];
-        const option3 = row[3];
-        const ans = row[4];
+            const row = element.split(',');
+            quest.push(row[0]);
+            console.log(row[0]);
+            option1.push(row[1]);
+            option2.push(row[2]);
+            option3.push(row[3]);
+            ans.push(row[4]);
 
-
-            let questions;
-         
-            questions = [
-                {
-                    numb: i,
-                    questions: option1,
-                    answer: ans,
-                    options: [
-                        option1,
-                        option2,
-                        option3
-                    ]
-                }
-            ];
-        
-    }
+        }
 
     )
 }
 
-// create array of number, questions, answer and options
-let questions = [
-    {
-        numb: 1,
-        question: "row",
-        answer: "correct answer",
-        options: [
-            "1",
-            "2",
-            "3",
-            
-        ]
-    },
-    {
-        numb: 2,
-        question: "second question?",
-        answer: "correct answer",
-        options: [
-            "correct answer",
-            "incorrect answer",
-            "incorrect answer",
-        ]
-    },
-    {
-        numb: 3,
-        question: "third question?",
-        answer: "correct answer",
-        options: [
-            "incorrect answer",
-            "correct answer",
-            "incorrect answer",
+async function question() {
+    await getData()
+    let questions = [
+        {
+            numb: 1,
+            questions: quest[0],
+            answer: ans[0],
+            options: [
+                option1[0],
+                option2[0],
+                option3[0]
+            ]
+        },
+        {
+            numb: 2,
+            questions: quest[1],
+            answer: ans[1],
+            options: [
+                option1[1],
+                option2[1],
+                option3[1]
+            ]
+        },
+        {
+            numb: 3,
+            questions: quest[2],
+            answer: ans[2],
+            options: [
+                option1[2],
+                option2[2],
+                option3[2]
+            ]
+        },
+        {
+            numb: 4,
+            questions: quest[3],
+            answer: ans[3],
+            options: [
+                option1[3],
+                option2[3],
+                option3[3]
+            ]
+        },
 
-        ]
-    },
-    {
-        numb: 4,
-        question: "fourth question?",
-        answer: "correct answer",
-        options: [
-            "incorrect answer",
-            "incorrect answer",
-            "correct answer",
-        ]
-    },
-    {
-        numb: 5,
-        question: "fifth question?",
-        answer: "correct answer",
-        options: [
-            "incorrect answer",
-            "correct answer",
-            "incorrect answer",
-        ]
-    },
-   
-];
-getData()
+    ];
+}*/
