90cdb6b961ff837b0ef01a214cc8c69eb89ad09f
b9019940 <b9019940@newcastle.ac.uk>
2021-01-19 14:30:37 +0000

    encrypting login details

--- AdminLogin.cs
+++ AdminLogin.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Data;
+using System.IO;
 using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Http;
 using MySqlX.XDevAPI.Relational;
+using Org.BouncyCastle.Crypto.Tls;
 using Ubiety.Dns.Core;
 
 namespace fdm_gamify2
@@ -17,6 +21,15 @@ namespace fdm_gamify2
             DataTable dataTable = databaseConnection.GetDataTable("SELECT * FROM AdminUsers");
             for (int i = 0; i < dataTable.Rows.Count; i++)
             {
+                Byte[] EncryptedUsername = Encoding.ASCII.GetBytes(Username);
+                Byte[] EncryptedPassword = Encoding.ASCII.GetBytes(Password);
+                MD5 md5 = new MD5CryptoServiceProvider();
+                md5.ComputeHash(EncryptedUsername);
+                EncryptedUsername = md5.Hash;
+                md5.ComputeHash(EncryptedPassword);
+                EncryptedPassword = md5.Hash;
+                Console.Out.WriteLine("\n" + System.Text.Encoding.ASCII.GetString(EncryptedUsername));
+                Console.Out.WriteLine(System.Text.Encoding.ASCII.GetString(EncryptedPassword) + "\n");
                 if (Username == dataTable.Rows[i]["Username"].ToString() && Password == dataTable.Rows[i]["Password"].ToString())
                 {
                     SessionManager session = new SessionManager();
