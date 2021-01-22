eb89d2c617e35808bf850664bea3276fe4b86a1f
b9019940 <b9019940@newcastle.ac.uk>
2021-01-22 10:48:56 +0000

    working on admin login

--- AdminLogin.cs
+++ AdminLogin.cs
@@ -28,21 +28,26 @@ namespace fdm_gamify2
             //Encrypting Username.
             md5.ComputeHash(EncryptedUsername);
             EncryptedUsername = md5.Hash;
+            Username = Encoding.ASCII.GetString(md5.Hash);
             //Encrypting Password.
             md5.ComputeHash(EncryptedPassword);
-            EncryptedPassword = md5.Hash;
-                
+            Password = Encoding.ASCII.GetString(md5.Hash);
+            Console.Out.WriteLine(Username);
+            Console.Out.WriteLine(Password);
             //Test by printing both encrypted values.
             //Console.Out.WriteLine("\n" + System.Text.Encoding.ASCII.GetString(EncryptedUsername));
             //Console.Out.WriteLine(System.Text.Encoding.ASCII.GetString(EncryptedPassword) + "\n");
 
             //Get every row for the database, and then check each row against log in details.
             DataTable dataTable = databaseConnection.GetDataTable("SELECT * FROM AdminUsers");
+            Console.Out.WriteLine(dataTable.Rows[0]["Username"].ToString());
+            Console.Out.WriteLine(dataTable.Rows[0]["Password"].ToString());
             for (int i = 0; i < dataTable.Rows.Count; i++)
             {
                 //Check if there's a match. If there is, then store the fact that they are an admin in the session.
-                if (Username == dataTable.Rows[i]["Username"].ToString() && Password == dataTable.Rows[i]["Password"].ToString())
+                if (Username == dataTable.Rows[i]["Username"] && Password == dataTable.Rows[i]["Password"])
                 {
+                    Console.Out.WriteLine("here");
                     SessionManager session = new SessionManager();
                     session.NewUser(httpContext, session, "true");
                     databaseConnection.CloseConnection();
